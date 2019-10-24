071ab53ff62ecc27c8395b831bc34b7e4232ddef
kw90 <kai.waelti@gmail.com>
2019-10-24 16:12:29 +0000

    Generated code by SwaggerHub

--- python-flask/swagger_server/test/test_developers_controller.py
+++ python-flask/swagger_server/test/test_developers_controller.py
@@ -6,6 +6,7 @@ from flask import json
 from six import BytesIO
 
 from swagger_server.models.basic_pose import BasicPose  # noqa: E501
+from swagger_server.models.inline_response200 import InlineResponse200  # noqa: E501
 from swagger_server.models.object import Object  # noqa: E501
 from swagger_server.test import BaseTestCase
 
@@ -20,10 +21,21 @@ class TestDevelopersController(BaseTestCase):
         """
         body = BasicPose()
         response = self.client.open(
-            '/kw90/known-pose-api/1.1.0/pose',
+            '/kw90/known-poses-api/1.1.0/poses',
             method='POST',
             data=json.dumps(body),
             content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_poses_id_delete(self):
+        """Test case for poses_id_delete
+
+        deletes a pose item
+        """
+        response = self.client.open(
+            '/kw90/known-poses-api/1.1.0/poses/{id}'.format(id='id_example'),
+            method='DELETE')
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
@@ -36,7 +48,7 @@ class TestDevelopersController(BaseTestCase):
                         ('skip', 1),
                         ('limit', 50)]
         response = self.client.open(
-            '/kw90/known-pose-api/1.1.0/pose',
+            '/kw90/known-poses-api/1.1.0/poses',
             method='GET',
             query_string=query_string)
         self.assert200(response,
