e7f240a3bc788a9d4f78cced5ec20624ce00300c
kw90 <kai.waelti@gmail.com>
2019-10-25 09:47:44 +0000

    Generated code by SwaggerHub

---
+++ typescript-angular/model/models.ts
@@ -0,0 +1,7 @@
+export * from './accuratePose';
+export * from './allPoses';
+export * from './basicPose';
+export * from './inaccuratePose';
+export * from './inlineResponse200';
+export * from './orientation';
+export * from './position';
